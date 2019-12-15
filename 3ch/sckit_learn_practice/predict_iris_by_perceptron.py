91e3b63b5ab956b1c289e42bbefbdbb9b0b24aa2
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2019-12-15 10:24:25 +0900

    Added sckit_lean_practice

---
+++ 3ch/sckit_learn_practice/predict_iris_by_perceptron.py
@@ -0,0 +1,35 @@
+import os, sys
+sys.path.append(os.getcwd() + "/../util")
+from decision_boudary import plot_decision_regions
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+# feature nomirization
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+from sklearn.linear_model import Perceptron
+ppn = Perceptron(n_iter = 40, eta0 = 0.1, random_state=0, shuffle=True)
+ppn.fit(X_train_std, y_train)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx = range(105, 150))
+
+plt.xlabel('petal length [standaized]')
+plt.ylabel('petal width [standaized]')
+plt.legend(loc='upper left')
+plt.show()
