e897a4948c650cd7c18c72c9c9a94e2f853d4204
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2019-12-29 16:00:48 +0900

    Added knn sample code

---
+++ 3ch/k_nearest/k_nearest.py
@@ -0,0 +1,34 @@
+import os, sys
+sys.path.append(os.getcwd() + "/../util")
+from decision_boudary import plot_decision_regions
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+# feature nomirization
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined, classifier=knn, test_idx=range(105, 150))
+plt.xlabel("petal length [standardized]")
+plt.ylabel("petal width [standardized]")
+plt.show()
